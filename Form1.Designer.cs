e2662e3ad2807b59244035a19855017eedd839f0
Budkovski <budkovskiy00@gmail.com>
2019-04-22 14:25:50 +0300

    Full zad

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox_zak2 = new System.Windows.Forms.TextBox();
+            this.textBox_zak1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button_enter = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button_cancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox_zak2
+            // 
+            this.textBox_zak2.Location = new System.Drawing.Point(62, 51);
+            this.textBox_zak2.Name = "textBox_zak2";
+            this.textBox_zak2.Size = new System.Drawing.Size(100, 20);
+            this.textBox_zak2.TabIndex = 0;
+            // 
+            // textBox_zak1
+            // 
+            this.textBox_zak1.Location = new System.Drawing.Point(62, 25);
+            this.textBox_zak1.Name = "textBox_zak1";
+            this.textBox_zak1.Size = new System.Drawing.Size(100, 20);
+            this.textBox_zak1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Od \r\n\r\nDo\r\n";
+            // 
+            // button_enter
+            // 
+            this.button_enter.Location = new System.Drawing.Point(87, 77);
+            this.button_enter.Name = "button_enter";
+            this.button_enter.Size = new System.Drawing.Size(75, 23);
+            this.button_enter.TabIndex = 2;
+            this.button_enter.Text = "Enter";
+            this.button_enter.UseVisualStyleBackColor = true;
+            this.button_enter.Click += new System.EventHandler(this.Button_enter_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(168, 25);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 368);
+            this.listBox1.TabIndex = 3;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(35, 399);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(253, 23);
+            this.progressBar1.TabIndex = 4;
+            this.progressBar1.Click += new System.EventHandler(this.ProgressBar1_Click);
+            // 
+            // button_cancel
+            // 
+            this.button_cancel.Location = new System.Drawing.Point(87, 107);
+            this.button_cancel.Name = "button_cancel";
+            this.button_cancel.Size = new System.Drawing.Size(75, 23);
+            this.button_cancel.TabIndex = 5;
+            this.button_cancel.Text = "Cancel";
+            this.button_cancel.UseVisualStyleBackColor = true;
+            this.button_cancel.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.button_cancel);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button_enter);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_zak1);
+            this.Controls.Add(this.textBox_zak2);
+            this.Name = "Form1";
+            this.Text = "Liczby pierwsze";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox_zak2;
+        private System.Windows.Forms.TextBox textBox_zak1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_enter;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button_cancel;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,120 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Mylibrary;
+using System.Threading;
 
 namespace WindowsFormsApp_liczby_pierwsze
 {
+
     public partial class Form1 : Form
+
     {
+       
+
+        delegate void listboxdel(int a);
+        delegate void progressbardel(int a);
         public Form1()
         {
             InitializeComponent();
+            cancelTokenSource = new CancellationTokenSource();
+            token = cancelTokenSource.Token;
+            
+        }
+        CancellationTokenSource cancelTokenSource;
+        CancellationToken token;
+        Task t, t2;
+
+        private void Button_enter_Click(object sender, EventArgs e)
+        {
+            t.Start();
+            //t.Wait();
+
+            //foreach (var item in numbers)
+            //{
+            //    listBox1.Items.Add(item);
+            //}
+        }
+        static List<int> numbers = new List<int>();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            t = new Task(() =>
+            {
+                int count = 0;
+                for (int i = textBox_zak1.Text.i(); i < textBox_zak2.Text.i(); i++)
+                {
+                    if (token.IsCancellationRequested)
+                    {
+                        MessageBox.Show("Cancelled");
+                        return;
+                    }
+                    count = 0;
+                    for (int h = 2; h < i; h++)
+                    {
+                        if (i%h==0)
+                        {
+                            count++;
+                        }
+
+                    }
+                    if (count==0)
+                    {
+                        
+                        numbers.Add(i);
+                    }
+                    Progressbaradd((i*100)/textBox_zak2.Text.i());
+                }
+               
+            });
+
+            t2 = t.ContinueWith((Task t) =>
+            {
+                foreach (var item in numbers)
+                {
+                    LIstboxadd(item);
+                }
+            });
+            
+
+            
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            cancelTokenSource.Cancel();
+        }
+
+        private void ProgressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LIstboxadd(int a)
+        {
+            if (InvokeRequired)
+            {
+                BeginInvoke(new listboxdel(LIstboxadd), new object[] { a });
+                return;
+            }
+            else
+            {
+                listBox1.Items.Add(a);
+            }
+        }
+
+
+        private void Progressbaradd(int a)
+        {
+            if (InvokeRequired)
+            {
+                BeginInvoke(new progressbardel(Progressbaradd), new object[] { a });
+                return;
+            }
+            else
+            {
+                progressBar1.Value = a;
+            }
         }
     }
+    
 }
